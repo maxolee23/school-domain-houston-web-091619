e37ab8ca9280afc97d1b2af6b54f9785b0c0e186
Max Lee <maxolee23@gmail.com>
2019-09-17 19:58:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,10 @@ class School
 
   def initialize(name)
     @name = name
-  end 
+    roster = {}
+  end
 
-end
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+        roster[grade] << student_name
+  end
